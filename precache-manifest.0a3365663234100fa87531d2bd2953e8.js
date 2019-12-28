26f1aa1c3d40299857a13e25ad455c8785a4f4be
Jesús José Sáenz Fierro <jesussaenz16180@gmail.com>
2019-12-28 09:14:25 -0700

    Updates

---
+++ precache-manifest.0a3365663234100fa87531d2bd2953e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12fde2db5ac8860847d075dac56dd1d",
+    "url": "/Notes/index.html"
+  },
+  {
+    "revision": "2e3edfccb16e380fe2d0",
+    "url": "/Notes/static/css/main.430a3b19.chunk.css"
+  },
+  {
+    "revision": "3805a9f21b10b91aaa57",
+    "url": "/Notes/static/js/2.794aa73f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Notes/static/js/2.794aa73f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e3edfccb16e380fe2d0",
+    "url": "/Notes/static/js/main.4b62d63a.chunk.js"
+  },
+  {
+    "revision": "f8b05bff705ad3c82fbb",
+    "url": "/Notes/static/js/runtime-main.d989c524.js"
+  }
+]);
